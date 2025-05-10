4e01e0fed76fc1b39b03381a42cf4c44a03c0228
Karl Mantle <130184376+karl-mantle@users.noreply.github.com>
2025-05-10 10:58:55 +0100

    chore: update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,35 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
+import icon from "astro-icon";
+import mdx from "@astrojs/mdx";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  site: "https://www.karlmantle.com",
+  integrations: [
+    icon({
+      iconDir: "src/assets/icons",
+      // add used icons here https://www.astroicon.dev/reference/configuration
+    }),
+    mdx(),
+    robotsTxt({
+      policy: [{
+        userAgent: '*',
+        disallow: '/'
+      }]
+    }),
+    sitemap(),
+  ],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
     },
   }),
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
