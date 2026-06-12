77fb2fa17754859884286ee624606542048ded0f
Karl Mantle <130184376+karl-mantle@users.noreply.github.com>
2026-06-12 23:59:53 +0100

    feat: alias path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tsconfigPaths()],
+  },
+});
