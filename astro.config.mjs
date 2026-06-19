bd00d697fc8520bc35863843e5ae8b0fe0dc63ff
Karl Mantle <130184376+karl-mantle@users.noreply.github.com>
2026-06-19 22:00:52 +0100

    feat: netlify form + small fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,27 @@
 import { defineConfig } from "astro/config";
 import { siteConfig } from "./src/site.config";
 import icon from "astro-icon";
+import netlify from "@astrojs/netlify";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://astro.build/config
 export default defineConfig({
   site: siteConfig.url,
+  adapter: netlify(),
   integrations: [
     icon({
       iconDir: "src/images/svg",
       include: {
-        "simple-icons": ["*"],
-        "material-symbols": ["*"],
+        "simple-icons": ["linkedin", "github"],
+        "material-symbols": [
+          "arrow-right-alt",
+          "check",
+          "content-copy-outline",
+          "mail-outline",
+          "rss-feed",
+          "share",
+        ],
       },
     }),
   ],
